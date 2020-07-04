297c45e0869aab6772fdb7c95753fb8f3d04062c
DavidStephenss <dvdstephensd@gmail.com>
2020-07-04 14:27:05 -0500

    aboutmedone

---
+++ script.js
@@ -0,0 +1,17 @@
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
+
+function myFunction () {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") { 
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
