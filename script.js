f692cf7c734b60db779c4591b22bbbfd1e214d28
DavidStephenss <dvdstephensd@gmail.com>
2020-07-04 16:37:11 -0500

    YAY

--- script.js
+++ script.js
@@ -7,11 +7,15 @@
 //   document.getElementById("myBar").style.width = scrolled + "%";
 // }
 
-function myFunction () {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") { 
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
+// function myFunction () {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") { 
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+$(window).scroll(function () {
+  $(".top").css("opacity", 1 - $(window).scrollTop() / 25);
+  });
